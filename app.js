64e9a2b89c7607147281c60ae921dd013db928a3
root <root@ip-172-31-1-158.ec2.internal>
2026-02-05 17:06:57 +0000

    Initial Node.js app

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Node.js Ubuntu EC2 pe chal raha hai 🚀');
+});
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
